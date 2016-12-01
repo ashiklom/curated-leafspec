cccfef75bdd317cb9e44ce1bb9ed1d7d20319b48
Alexey Shiklomanov <ashiklom@bu.edu>
2016-12-01 15:57:11 -0500

    Bugfixes in NGEE-Arctic processing

--- process.ngee_arctic.R
+++ process.ngee_arctic.R
@@ -52,10 +52,10 @@ speclist[['2016b']] <- read_excel(spec_2016b_file, sheet = 1) %>%
 
 spec_2016s_file <- file.path(path_nga, "2016_Data",
                              "NGEE-Arctic_Seward_2016_HR1024i_Leaf_Spectral_Reflectance.xlsx")
-speclist[['2016s']] <- read_excel(spec_2016b_file, sheet = 1) %>%
+speclist[['2016s']] <- read_excel(spec_2016s_file, sheet = 2) %>%
     setDT() %>%
-    setnames('Sample_Barcode', 'SampleName') %>%
-    mutate(SampleYear = 2016, Site = 'Seward') %>%
+    setnames('BNL_Barcode', 'SampleName') %>%
+    mutate(SampleYear = 2016, Site = 'Seward_Kougarok') %>%
     scale_wl()
 
 specdat_full <- rbindlist(speclist, fill = TRUE)
@@ -87,7 +87,8 @@ traits_main <- read_excel(traits_main_file, sheet = 2) %>%
     setnames(names(traits_namesdict), traits_namesdict) %>%
     setDT() %>%
     mutate(SampleYear = year(strptime(Measurement_Date, "%Y%m%d")),
-           DOY = yday(strptime(Measurement_Date, "%Y%m%d")))
+           DOY = yday(strptime(Measurement_Date, "%Y%m%d")),
+           Site = 'Barrow')
 
 # Load pigment data for 2015
 chl_convert <- 1000 / (100^2)
@@ -99,15 +100,61 @@ pigments <- read_excel(pigments_file, sheet = 2) %>%
            leaf_chlorophyll_b = Chl_b_mg_m2 * chl_convert,
            leaf_chlorophyll_total = Chl_a_plus_b_mg_m2 * chl_convert,
            leaf_carotenoid_total = Tot_Car_mg_m2 * chl_convert, 
-           SampleYear = 2015) %>%
+           SampleYear = 2015, 
+           Site = 'Barrow') %>%
     setDT()
 
-nga_dat <- specdat %>%
-    left_join(traits_main, by = c('SampleName', 'SampleYear')) %>%
-    left_join(pigments, by = c('SampleName', 'SampleYear')) %>%
-    mutate(Project = projectcode) %>%
-    mutate(FullName = paste(Project, SampleName, SampleYear, 
-                            sep = id_separator)) %>%
-    subToCols()
+# Load other data from 2016
+seward_2016_lma_file <- file.path(path_nga, "2016_Data", 
+                                  "2016KGsamples_LMA-edited.csv")
+seward_2016_lma <- fread(seward_2016_lma_file) %>%
+    mutate(SampleName = paste0("BNL", Sample_Barcode), 
+           SampleYear = 2016,
+           DOY = yday(strptime(Measurement_Date, "%Y%m%d"))) %>%
+    rename(leaf_mass_per_area = LMA_gDW_m2,
+           RawSpecies = USDA_Species_Code) %>%
+    setDT()
 
+barrow_2016_chn_file <- file.path(path_nga, "2016_Data", 
+                                  "Barrow2016_CHN_analysis-edited.xlsx")
+barrow_2016_chn <- read_excel(barrow_2016_chn_file, sheet = 2) %>%
+    rename(SampleName = Sample_Barcode,
+           RawSpecies = USDA_Species_Code,
+           leaf_C_percent = Perc_C,
+           leaf_N_percent = Perc_N,
+           leaf_CN_ratio = CN_ratio) %>%
+    mutate(SampleYear = 2016,
+           Site = 'Barrow') %>%
+    setDT()
+
+barrow_2016_lma_file <- file.path(path_nga, "2016_Data", 
+                                  "Barrow2016_samples_LMA.csv")
+barrow_2016_lma <- fread(barrow_2016_lma_file) %>%
+    rename(leaf_mass_per_area = LMA_gDW_m2,
+           RawSpecies = Species) %>%
+    mutate(SampleName = paste0("BNL", Sample_Barcode),
+           SampleYear = 2016, 
+           DOY = yday(strptime(Measurement_Date, "%Y%m%d")),
+           Site = 'Barrow') %>%
+    setDT()
+
+dat_2016 <- full_join(seward_2016_lma, barrow_2016_lma) %>%
+    full_join(barrow_2016_chn) %>%
+    subToCols()
+dat_other <- full_join(traits_main, pigments) %>%
+    filter(!is.na(SampleYear))
+traits_full <- full_join(dat_2016, dat_other)
+
+nga_dat <- specdat %>%
+    left_join(traits_full, by = c('SampleName', 'SampleYear', 'Site')) %>%
+    mutate(Project = projectcode,
+           FullName = paste(Project, SampleName, SampleYear, 
+                            sep = id_separator))
+
+# Sanity checks
+nga_dat %>% group_by(Site) %>% count()
+nga_dat %>% group_by(RawSpecies) %>% count()
+nga_dat %>% filter(is.na(RawSpecies)) %>% 
+    group_by(Site, SampleYear) %>% count()
+ 
 saveRDS(nga_dat, file = rds_name(projectcode))
