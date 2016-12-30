8c9a8f244e84697ee12b1b2b21aaec45c828bb5a
Alexey Shiklomanov <ashiklom@bu.edu>
2016-12-30 13:24:29 -0500

    Finish processing NASA FFT

--- process.nasa_fft.R
+++ process.nasa_fft.R
@@ -4,9 +4,9 @@
 #' ---
 
 #' # Setup
-source("common.R")
-
-project_code <- "NASA_FFT"
+library(specprocess)
+projectcode <- "nasa_fft"
+specdb <- src_postgres('leaf_spectra')
 
 #' Set paths for FFT data
 PATH.FFT <- file.path("raw","NASA_FFT")
@@ -16,49 +16,13 @@ PATH.FFT <- file.path("raw","NASA_FFT")
 PATH.spec <- file.path(PATH.FFT, "spec")
 PATH.refl <- file.path(PATH.spec, "NASA_FFT_LC_Refl_Spectra_v4.csv")
 
-#' Load reflectance data 
-nasa_fft.all_refl <- fread(PATH.refl, header=TRUE) %>%
-    .[, Project := project_code] %>%
-    .[, FullName := paste(project_code, Sample_Name, Sample_Year,
-                             sep = id_separator)] %>%
-    setnames("Species", "RawSpecies")
-
-if (any(duplicated(nasa_fft.all_refl[, FullName]))) {
-    stop("FullName is not unique")
-}
-
-#' Isolate full sample information
-
-all.cols <- colnames(nasa_fft.all_refl)
-wl.cols <- grep("Wave_", all.cols, value = TRUE)
-info.cols <- all.cols[!(all.cols %in% wl.cols)]
-nasa_fft.all <- nasa_fft.all_refl[, info.cols, with = F]
-
-#' Convert spectra into list of matrices
-
-reflFromDT <- function(dat, colname) {
-    rowname_vec <- dat[, FullName]
-    wl.cols <- grep("Wave_", colnames(dat), value = TRUE)
-    wavelengths <- as.numeric(sub("Wave_", "", wl.cols))
-    dat[, wl.cols, with = F] %>%
-        as.matrix %>% 
-        t %>%
-        "colnames<-"(rowname_vec) %>%
-        apply(2, function(x) list(as.matrix(x))) %>%
-        lapply(function(x) cbind(wavelengths, x[[1]])) %>%
-        lapply("colnames<-", c("Wavelength", colname)) %>%
-        lapply("rownames<-", NULL) %>%
-        lapply(specobs)
-}
-
-nasa_fft.refl <- reflFromDT(nasa_fft.all_refl, "Reflectance")
-nasa_fft.all <- nasa_fft.all[, Reflectance := nasa_fft.refl[FullName]]
-
-
-#' Correct column names
+#' Set names for fixing
 names_samples <- c("Sample_Name" = "SampleName",
-                   "Sample_Year" = "SampleYear",
-                   "Height" = "CanopyPosition")
+                   "Sample_Year" = "year",
+                   "Height" = "CanopyPosition",
+                   'Site' = 'sitecode',
+                   'Plot' = 'plotcode',
+                   'Species' = 'datacode')
 
 names_specInfo <- c("Instrumentation" = "Instrument",
                     "Measurement_Type" = "Apparatus",
@@ -66,7 +30,18 @@ names_specInfo <- c("Instrumentation" = "Instrument",
 
 names_all <- c(names_samples, names_specInfo)
 
-setnames(nasa_fft.all, names(names_all), names_all)
+#' Load reflectance data 
+nasa_fft.all_refl <- fread(PATH.refl, header=TRUE) %>%
+    mutate(projectcode = projectcode,
+           samplecode = paste(projectcode, Sample_Name, Sample_Year,
+                              sep = '|')) %>%
+    setnames(names(names_all), names_all)
+
+if (any(duplicated(nasa_fft.all_refl[, samplecode]))) {
+    stop("samplecode is not unique")
+}
+
+#' Isolate full sample information
 
 #' Sort out needle age variables
 fixNeedleAge <- function(dat) {
@@ -80,21 +55,34 @@ fixNeedleAge <- function(dat) {
     return(out)
 }
 
-nasa_fft.all <- fixNeedleAge(nasa_fft.all)
+samples_refl <- select(nasa_fft.all_refl, -starts_with('Wave_')) %>%
+    fixNeedleAge()
+spectra_refl <- select(nasa_fft.all_refl, samplecode, starts_with('Wave_')) %>%
+    melt(id.vars = 'samplecode') %>%
+    mutate(wavelength = as.numeric(gsub('Wave_', '', variable))) %>%
+    select(-variable)
+spectra_info_refl <- distinct(spectra_refl, samplecode) %>%
+    mutate(type = 'reflectance')
 
 #' Repeat above steps for transmittance
 message("Loading transmittance...")
 PATH.trans <- file.path(PATH.spec, "NASA_FFT_IS_Tran_Spectra_v4.csv")
-nasa_fft.trans <- fread(PATH.trans, header=TRUE) %>%
-    .[, FullName := paste(Project, Sample_Name, Sample_Year, 
-                              sep = id_separator),] %>%
-    .[, Transmittance := reflFromDT(., "Transmittance")] %>%
-    setnames("Species", "RawSpecies") %>%
-    .[, list(FullName, Transmittance, RawSpecies)]
+nasa_fft.trans_all <- fread(PATH.trans, header=TRUE) %>%
+    setnames(names(names_all), names_all) %>%
+    mutate(samplecode = paste(projectcode, SampleName, year, 
+                              sep = '|'))
 
-nasa_fft.all <- merge(nasa_fft.all, nasa_fft.trans, 
-                      by = c("FullName", "RawSpecies"), 
-                      all = TRUE)
+samples_trans <- select(nasa_fft.trans_all,
+                        samplecode, SampleName, year,
+                        sitecode, plotcode, datacode,
+                        CanopyPosition, Age) %>%
+    fixNeedleAge()
+spectra_trans <- select(nasa_fft.trans_all, samplecode, starts_with('Wave_')) %>%
+    melt(id.vars = 'samplecode') %>%
+    mutate(wavelength = as.numeric(gsub('Wave_', '', variable))) %>%
+    select(-variable)
+spectra_info_trans <- distinct(spectra_trans, samplecode) %>%
+    mutate(type = 'transmittance')
 
 #' # Process chemistry data
 #' Set paths
@@ -110,13 +98,13 @@ PATH.SLA_LMA <- file.path(PATH.FFT,
 
 #' Read in data
 nasa_fft.d15N <- fread(PATH.d15N, header=TRUE) %>%
-    setnames('SPECIES', 'RawSpecies')
+    setnames('SPECIES', 'datacode')
 nasa_fft.lignin <- fread(PATH.lignin, header=TRUE) %>%
-    setnames('SPECIES', 'RawSpecies')
+    setnames('SPECIES', 'datacode')
 nasa_fft.cn <- fread(PATH.CN, header=TRUE) %>%
-    setnames('Species', 'RawSpecies')
+    setnames('Species', 'datacode')
 nasa_fft.lma <- fread(PATH.SLA_LMA, header=TRUE) %>%
-    setnames('Species', 'RawSpecies')
+    setnames('Species', 'datacode')
 
 #' Remove data with comments, which usually indicate that there's something 
 #' wrong with the data. TODO: Look more closely into this.
@@ -125,12 +113,13 @@ nasa_fft.lignin <- nasa_fft.lignin[COMMENTS == ""]
 
 #' Set negative values of LMA to NA. Negative values make no sense for EWT and 
 #' LMA.
-nasa_fft.lma[EWT_g_cm2 < 0, EWT_g_cm2 := NA]
-nasa_fft.lma[LMA_g_DW_m2 < 0, LMA_g_DW_m2 := NA]
+nasa_fft.lma <- nasa_fft.lma %>%
+    mutate(EWT_g_cm2 = if_else(EWT_g_cm2 < 0, NA_real_, EWT_g_cm2),
+           LMA_g_DW_m2 = if_else(LMA_g_DW_m2 < 0, NA_real_, LMA_g_DW_m2))
 
 #' Extract only the values that we're interested in from each data.table.
-mergeby.lower <- c("Sample_Name", "Sample_Year", 'RawSpecies')
-mergeby.caps <- c('SAMPLE_NAME', 'SAMPLE_YEAR', 'RawSpecies')
+mergeby.lower <- c("Sample_Name", "Sample_Year", 'datacode')
+mergeby.caps <- c('SAMPLE_NAME', 'SAMPLE_YEAR', 'datacode')
 nasa_fft.d15N <- nasa_fft.d15N[, c(mergeby.caps, "SAMPLE_dN15"), with=F]
 nasa_fft.lignin <- nasa_fft.lignin[, c(mergeby.caps, "ADF_PERC_DW", "ADL_PERC_DW",
                              "CELL_PERC_DW"), with=F]
@@ -160,37 +149,77 @@ setkeyv(nasa_fft.lma, mergeby.lower)
 merge.caps <- merge(nasa_fft.d15N, nasa_fft.lignin, all=T)
 merge.lower <- merge(nasa_fft.cn, nasa_fft.lma, all=T)
 setnames(merge.caps, mergeby.caps, mergeby.lower)
-nasa_fft.traits <- merge(merge.caps, merge.lower, by=mergeby.lower, all=T)
-check.unique(nasa_fft.traits, mergeby.lower)
+nasa_fft.traits <- merge(merge.caps, merge.lower, by=mergeby.lower, all=T) %>%
+    mutate(leaf_water_thickness = ud.convert(EWT_g_cm2, 'g cm-2', 'kg m-2'),
+           leaf_mass_per_area = ud.convert(LMA_g_DW_m2, 'g m-2', 'kg m-2')) %>%
+    rename(leaf_C_pct_mass = Perc_C,
+           leaf_N_pct_mass = Perc_N,
+           leaf_CN_ratio_mass = CNRatio,
+           leaf_cellulose_pct_mass = CELL_PERC_DW,
+           leaf_lignin_pct_mass = ADL_PERC_DW,
+           leaf_fiber_pct_mass = ADF_PERC_DW,
+           leaf_dN15 = SAMPLE_dN15,       # TODO: Figure out unit!
+           SampleName = Sample_Name,
+           year = Sample_Year) %>%
+    mutate(samplecode = paste(projectcode, SampleName, year, sep = '|'))
+samples_traits <- nasa_fft.traits %>%
+    select(samplecode, SampleName, year)
 
-#' Perform unit conversions
-nasa_fft.traits[,leaf_water_content := EWT_g_cm2 * 100^2]  # Convert to g m-2
-nasa_fft.traits[,EWT_g_cm2 := NULL]    # Remove unused column
-setnames(nasa_fft.traits, "LMA_g_DW_m2", "leaf_mass_per_area")    # No conversion
-setnames(nasa_fft.traits, "Perc_C", "leaf_C_percent")      # No conversion
-setnames(nasa_fft.traits, "Perc_N", "leaf_N_percent")      # No conversion
-setnames(nasa_fft.traits, "CNRatio", "leaf_CN_ratio")  # No conversion
-# No protein
-setnames(nasa_fft.traits, "CELL_PERC_DW", "leaf_cellulose_percent")
-setnames(nasa_fft.traits, "ADL_PERC_DW", "leaf_lignin_percent")
-# No starch
-setnames(nasa_fft.traits, "ADF_PERC_DW", "leaf_fiber_percent")
-setnames(nasa_fft.traits, "SAMPLE_dN15", "leaf_deltaN15")     # TODO: Not sure about this unit
-setnames(nasa_fft.traits, c("Sample_Name", "Sample_Year"),
-         c("SampleName", "SampleYear"))
+#' # Set up SQL tables
+samples_all <- samples_refl %>%
+    left_join(anti_join(samples_trans, samples_refl, 'samplecode')) %>%
+    full_join(samples_traits) %>%
+    left_join(tbl(specdb, 'species_dict') %>% 
+              select(-id) %>% 
+              collect() %>% 
+              setDT()) %>%
+    select(-datacode) %>%
+    mutate(projectcode = 'nasa_fft',
+           sitecode = paste(projectcode, sitecode, sep = '.'),
+           plotcode = paste(sitecode, plotcode, sep = '.'))
 
-#' Combine, organize, and save
-traitcols <- c("leaf_water_content", "leaf_mass_per_area", 
-               "leaf_C_percent", "leaf_N_percent", "leaf_CN_ratio")
-nasa_fft <- merge(nasa_fft.all, nasa_fft.traits,
-                  all = TRUE,
-                  by = c("SampleName", "SampleYear", 'RawSpecies')) %>% 
-    .[is.na(Project), Project := project_code] %>%
-    .[is.na(FullName), FullName := paste(project_code, SampleName, SampleYear, 
-                                         sep = id_separator)] %>%
-    .[, (traitcols) := lapply(.SD, mean, na.rm = TRUE), by = FullName,
-      .SDcols = traitcols] %>%
-    .[!duplicated(FullName)] %>%
-    subToCols
+sites <- distinct(samples_all, sitecode) %>% 
+    rename(code = sitecode)
+merge_with_sql(sites, 'sites')
 
-saveRDS(nasa_fft, file = "processed-spec-data/nasa_fft.rds")
+# TODO: Add latitude and longitude
+plots <- distinct(samples_all, plotcode) %>%
+    rename(code = plotcode)
+merge_with_sql(plots, 'plots')
+
+samples <- samples_all %>%
+    select(projectcode, code = samplecode, year, 
+           sitecode, plotcode, speciescode)
+merge_with_sql(samples, 'samples')
+
+spectra_info <- left_join(spectra_info_refl, spectra_info_trans)
+merge_with_sql(spectra_info, 'spectra_info', 'samplecode')
+
+specid <- tbl(specdb, 'spectra_info') %>%
+    rename(spectraid = id) %>%
+    right_join(tbl(specdb, 'samples') %>% 
+               filter(projectcode == 'nasa_fft') %>%
+               select(samplecode = code)) %>%
+    select(spectraid, samplecode) %>%
+    collect() %>%
+    setDT()
+
+spectra_data <- spectra_refl %>%
+    left_join(spectra_trans) %>%
+    left_join(specid) %>%
+    select(-samplecode)
+merge_with_sql(spectra_data, 'spectra_data', 'spectraid')
+
+traits <- nasa_fft.traits %>%
+    select(samplecode, starts_with('leaf')) %>%
+    melt(id.vars = 'samplecode', variable.name = 'trait', na.rm = TRUE)
+
+trait_info <- traits %>%
+    distinct(trait) %>%
+    mutate(unit = case_when(grepl('pct_mass', .$trait) ~ '%',
+                            grepl('_per_area|thickness', .$trait) ~ 'kg m-2',
+                            grepl('_ratio', .$trait) ~ 'unitless',
+                            TRUE ~ NA_character_))
+
+merge_with_sql(trait_info, 'trait_info', 'trait')
+merge_with_sql(traits, 'trait_data', 'samplecode')
