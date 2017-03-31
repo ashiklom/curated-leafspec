7b58889d31c9f104ffdd78646559099ce97d7bc7
Alexey Shiklomanov <ashiklom@bu.edu>
2017-03-31 17:00:34 -0400

    Clean Foster beetle dataset

--- process.foster_beetle.R
+++ process.foster_beetle.R
@@ -8,12 +8,14 @@ datapath <- '~/Dropbox/NASA_TE_PEcAn-RTM_Project/Data/Foster_etal/SBMP2_contrem_
 
 projects <- tibble(
     projectcode = 'foster_beetle',
-    projectdescription = 'Foster et al. 2017 (FEM) bark beetle spectra study',
+    projectshortname = 'Foster et al. 2017 FEM',
+    projectdescription = paste('Foster et al. 2017.',
+                               'Spectral evidence of early-stage spruce beetle infestation in Engelmann spruce',
+                               'Forest Ecology and Management'),
     pointofcontact = 'Foster, Adrianna',
     email = 'acf7m@virginia.edu',
     doi = '10.1016/j.foreco.2016.11.004') %>%
-    db_merge_into(db = specdb, table = 'projects', values = .,
-                  by = 'projectcode', id_colname = 'projectid')
+    write_project()
 
 siteplot <- tibble(
     sitecode = 'foster_beetle.MP',
@@ -24,10 +26,8 @@ siteplot <- tibble(
     longitude = -106 - (20/60) - (8.1594/3600)) %>%
     mutate(plotcode = sitecode,
            plotdescription = sitedescription) %>%
-    db_merge_into(db = specdb, table = 'sites', values = .,
-                  by = 'sitecode', id_colname = 'siteid') %>%
-    db_merge_into(db = specdb, table = 'plots', values = .,
-                  by = 'plotcode', id_colname = 'plotid')
+    db_merge_into(db = specdb, table = 'sites', values = ., by = 'sitecode') %>%
+    db_merge_into(db = specdb, table = 'plots', values = ., by = 'plotcode')
 
     
 rawdat <- read_csv(datapath)
@@ -35,45 +35,42 @@ rawdat <- read_csv(datapath)
 samples <- rawdat %>%
     select(TREEID, Height, Infested) %>%
     mutate(speciescode = 'PIEN',
-           projectcode = projects$projectcode,
-           plotcode = siteplot$plotcode,
+           projectcode = projects[['projectcode']],
+           plotcode = siteplot[['plotcode']],
            year = 2014,
            collectiondate = as.Date('2014-09-05'),
            spectra_name = paste(TREEID, Height, sep = '_'),
            samplecode = paste(projectcode, spectra_name, year, sep = '|')) %>%
-    db_merge_into(db = specdb, table = 'samples', values = .,
-                  by = 'samplecode', id_colname = 'sampleid')
+    db_merge_into(db = specdb, table = 'samples', values = ., by = 'samplecode')
 
-sample_condition_info <- tibble(
-    condition = 'bark_beetle_infested',
-    conditiondescription = paste('Tree infested (yes/no) by spruce beetle', 
-                                 '(Dendroctonus rufipennis (Kirby))'),
-    conditioncomment = 'See Foster et al. (2017) FME') %>%
-    db_merge_into(db = specdb, table = 'sample_condition_info', values = .,
-                  by = 'condition', id_colname = 'conditionid')
+sample_condition_info <- tribble(
+    ~condition, ~conditiondescription, ~conditioncomment,
+    'bark_beetle_infested', 'Tree infested (yes/no) by spruce beetle, (Dendroctonus rufipennis (Kirby))', 'See foster et al. (2017) FEM',
+    'canopyposition', 'Relative position within the canopy (bottom, middle, or top)', NA,
+    'sunshade', 'Whether the leaf is fully sunlit (sun) or at least partially shaded (shade)', NA) %>% 
+    db_merge_into(db = specdb, table = 'sample_condition_info', values = ., by = 'condition')
 
 sample_condition <- samples %>%
     mutate(bark_beetle_infested = recode(Infested, `Y` = 'yes', `N` = 'no'),
-           CanopyPosition = recode(Height, `H` = 'M', `L` = 'B'),
+           canopyposition = recode(Height, `H` = 'M', `L` = 'B'),
            sunshade = 'shade') %>%
-    select(samplecode, bark_beetle_infested, CanopyPosition, sunshade) %>%
+    select(samplecode, bark_beetle_infested, canopyposition, sunshade) %>%
     gather(condition, conditionvalue, -samplecode, na.rm = TRUE) %>%
-    db_merge_into(db = specdb, table = 'sample_condition', values = .,
-                  by = c('samplecode', 'condition'), id_colname = 'conditiondataid')
+    db_merge_into(db = specdb, table = 'sample_condition', values = ., by = c('samplecode', 'condition'))
 
 specmethods <- tibble(
+    instrumentcode = 'asd-fieldspec-pro',
+    specmethodcode = 'foster-method',
     instrumentname = 'ASD FieldSpec Pro',
     apparatus = 'Leaf clip',
     specmethodcomment = 'Foster et al. (2017) FEM') %>%
-    db_merge_into(db = specdb, table = 'instruments', values = .,
-                  by = 'instrumentname', id_colname = 'instrumentid') %>%
+    db_merge_into(db = specdb, table = 'instruments', values = ., by = 'instrumentcode') %>%
     db_merge_into(db = specdb, table = 'specmethods', values = .,
-                  by = c('instrumentid', 'apparatus', 'specmethodcomment'),
-                  id_colname = 'specmethodid')
+                  by = c('instrumentcode', 'apparatus', 'specmethodcode'))
 
 spectra_info <- samples %>%
     select(samplecode) %>%
-    mutate(specmethodid = specmethods$specmethodid,
+    mutate(specmethodcode = specmethods[['specmethodcode']],
            spectratype = 'continuum-removed reflectance') %>%
     db_merge_into(db = specdb, table = 'spectra_info', values = .,
                   by = c('samplecode', 'spectratype'), id_colname = 'spectraid')
